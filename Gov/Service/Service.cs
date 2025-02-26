0d00cd1c00558071004c0a2b299551b97805c80c
EthanNCai <chickenbilibili@outlook.com>
2025-02-26 22:23:03 +0800

    feat: service\policy\action

---
+++ Gov/Service/Service.cs
@@ -0,0 +1,21 @@
+using System.Security.Cryptography;
+using UnityEditor.Search;
+
+
+public enum ConsecuntServiceType{
+    TestService,
+}
+
+
+// Service 有几种？
+
+public class Service
+{
+    public string serviceName;
+    public int unitCost;
+    public Service(){
+
+    }
+    
+
+}
